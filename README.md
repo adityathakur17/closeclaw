d4592756e09eaab3a0ded8109b1b378037df1e1d
Aditya Thakur <adityathakur1709@gmail.com>
2026-06-04 18:39:55 +0530

    feat: telegram bot functionality

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+hello from telegram
+
 # close-claw
 
 To install dependencies:
--- tui/wakeup.ts
+++ tui/wakeup.ts
@@ -2,6 +2,7 @@ import { select, isCancel } from "@clack/prompts";
 import chalk from "chalk"
 import figlet from "figlet"
 import { runCliMode } from "../modes/cli";
+import { runTelegramMode } from "../modes/telegram";
 
 const BANNER_FONT = 'ANSI Shadow';
 const SHADOW = chalk.hex('#05480d')
@@ -50,6 +51,6 @@ export async function runWakeup() {
         await runCliMode()
     }
     else if (mode === 'telegram'){
-        console.log(chalk.dim("Starting telegram mode..."))
+        await runTelegramMode()
     }
 }
